3b16c8fd1e849bad24f3ea8ef0869de25f822e93
VladislavAndreev <andreev.92@inbox.ru>
2023-06-12 14:00:54 +0300

    add 1 Task

---
+++ Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Напишите первое число");
+string input_string1 = Console.ReadLine();
+Console.WriteLine("Напишите второе число");
+string input_string2 = Console.ReadLine();
+int number1 = Convert.ToInt32(input_string1);
+int number2 = Convert.ToInt32(input_string2);
+if (number2 > number1)
+{
+    Console.WriteLine($"Число {number2} является Max, {number1} является Min");
+
+}
+else
+{
+    Console.WriteLine($"Число {number1} является Max, {number2} является Min");
+}
