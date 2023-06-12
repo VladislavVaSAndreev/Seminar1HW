23975cb247566ecd452845f55db507161ae82a60
VladislavAndreev <andreev.92@inbox.ru>
2023-06-12 14:21:52 +0300

    add Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Напишите первое число");
+string input_string1 = Console.ReadLine();
+Console.WriteLine("Напишите второе число");
+string input_string2 = Console.ReadLine();
+Console.WriteLine("Напишите третье число");
+string input_string3 = Console.ReadLine();
+int number1 = Convert.ToInt32(input_string1);
+int number2 = Convert.ToInt32(input_string2);
+int number3 = Convert.ToInt32(input_string3);
+int max = number1;
+if (number1 > max) max = number1;
+if (number2 > max) max = number2;
+if (number3 > max) max = number3;
+Console.WriteLine($"Max являестся {max}");
