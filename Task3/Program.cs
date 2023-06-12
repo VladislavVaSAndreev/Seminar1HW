4959c98a0dfcebabb12284ce13b328d8b1d65cbc
VladislavAndreev <andreev.92@inbox.ru>
2023-06-12 14:44:32 +0300

    add Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Напишите число");
+string input_string1 = Console.ReadLine();
+int number1 = Convert.ToInt32(input_string1);
+if (number1%2 == 0)
+{
+    Console.WriteLine($"Число {number1} является четным");
+
+}
+else
+{
+    Console.WriteLine($"Число {number1} не является четным");
+}
