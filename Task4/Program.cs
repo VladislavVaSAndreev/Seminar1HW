bb6175839626dd53ca462037cef2e2715d5e2d07
VladislavAndreev <andreev.92@inbox.ru>
2023-06-12 15:44:44 +0300

    add Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Напишите число");
+string input_string1 = Console.ReadLine();
+int number1 = Convert.ToInt32(input_string1);
+int number2 = 2;
+while (number2 <= number1)
+{   
+    Console.Write(number2);
+    number2++;
+    number2++;
+}
